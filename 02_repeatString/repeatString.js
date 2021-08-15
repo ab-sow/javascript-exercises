eb6bbf5a4c70ed2b1a886288ff0fa65b3ec66710
ab-sow <62764353+ab-sow@users.noreply.github.com>
2021-08-15 13:09:08 +0100

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  var stringRepeat = '';
+  while (num > 0) {
+    stringRepeat += string;
+  }
+  return stringRepeat;
 };
-
+repeatString('hey', 4);
 module.exports = repeatString;
