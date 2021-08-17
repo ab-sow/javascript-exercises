7dd4a72838ebb7a9f6de4602c91f09e2d949c9f1
ab-sow <62764353+ab-sow@users.noreply.github.com>
2021-08-17 10:33:24 +0100

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
 const reverseString = function(str) {
-
+ var splitString = str.split("");
+ var reverseArry = splitString.reverse();
+ var joinArry = reverseArry.join("");
+  return jointArry;
 };
-
+reverseString("hello"):
 module.exports = reverseString;
