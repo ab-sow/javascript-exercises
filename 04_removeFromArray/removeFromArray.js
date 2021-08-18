bc8ee3a87bdd67fa9bf5d94c7ab0e9670087152b
ab-sow <62764353+ab-sow@users.noreply.github.com>
2021-08-18 07:28:36 +0100

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+ for(i = 0; i < arr.length; i++) {
+   if(arr[i] === 5) {
+    arr.splice(i, 1); 
+   }
+   retun arr;
+ }
 };
-
+removeFromArray(1, 2, 3, 4, 5, 6);
 module.exports = removeFromArray;
